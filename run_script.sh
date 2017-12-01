76a9d2ed0f012e6e8fbd4ac085267f52f671d4eb
christopher snow <chsnow123@gmail.com>
2017-12-01 14:58:30 +0000

    disable debug

--- run_script.sh
+++ run_script.sh
@@ -10,4 +10,12 @@ cd /home/jovyan/work
      --NotebookApp.ip=0.0.0.0 \
      --NotebookApp.session_manager_class=nb2kg.managers.SessionManager \
      --NotebookApp.kernel_manager_class=nb2kg.managers.RemoteKernelManager \
-     --NotebookApp.kernel_spec_manager_class=nb2kg.managers.RemoteKernelSpecManager 2>&1
+     --NotebookApp.kernel_spec_manager_class=nb2kg.managers.RemoteKernelSpecManager 
+
+# . /usr/local/bin/start.sh jupyter notebook --debug \
+#      --no-browser \
+#      --port=$NB_PORT \
+#      --NotebookApp.ip=0.0.0.0 \
+#      --NotebookApp.session_manager_class=nb2kg.managers.SessionManager \
+#      --NotebookApp.kernel_manager_class=nb2kg.managers.RemoteKernelManager \
+#      --NotebookApp.kernel_spec_manager_class=nb2kg.managers.RemoteKernelSpecManager 2>&1
